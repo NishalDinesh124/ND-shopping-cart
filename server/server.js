13c05c66bae92e21b214dbad93a5b7fea9f5ed08
Nishal Dinesh <nishaldinesh3516j@gmail.com>
2025-06-06 00:56:51 +0530

    UI improvements done

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express();
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://nd-shopping-cart.vercel.app',
   credentials: true,
 }));
 app.use(express.json());
